88dc74526c39126fc7a33b2651c150973dc38e82
captain3916 <guoningdong2@163.com>
2018-12-26 19:24:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import './components/common/css/move-base.css';
+import './components/common/css/main.css';
+
+import City from "./components/views/citys";
+import Home from "./components/views/home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <Switch>
+          <Route path='/films' component={ Home }></Route>
+          <Route path='/citys' component={ City }></Route>
+          <Redirect to='/films'></Redirect>
+        </Switch>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
